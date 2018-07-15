33fa5b212c79cb806218870096533dc3567d9e1f
chen8000 <13126618000@163.com>
2018-07-15 15:04:15 +0800

    hello React

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+
+// 自定义组建
+import App from './components/App';
+
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
