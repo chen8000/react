676892e8beac5c6515cc1be4fe1c71e29d1c934d
chen8000 <13126618000@163.com>
2018-07-15 19:15:44 +0800

    state  props ref

--- my-app/src/components/App.js
+++ my-app/src/components/App.js
@@ -1,9 +1,48 @@
 import React, { Component } from 'react';
 
 class App extends Component {
+
+  constructor(props){
+    super(props)
+
+    this.state = {
+      btnText : '点击',
+      input1Value : ''
+    }
+
+    // 把自定义函数的this绑定成组件的this
+    this.btnFn = this.btnFn.bind(this);
+    this.blurFn = this.blurFn.bind(this);
+  }
+
+  // 点击获取input的值
+  btnFn(){
+    // const input1Val = this.refs.input1.value;
+    // console.log(input1Val)
+
+    // 建议的写法
+    console.log(this.input1.value)
+  }
+
+  // 失去焦点获取值
+  blurFn(event){
+    // 可以这样获取
+    console.log(event.target.value);
+
+    // 也可以这样
+    console.log(this.input2.value)
+
+  }
+
   render() {
     return (
-      <h1>hellow React</h1>
+      <div>
+        <h1>hellow React</h1>
+        <input type="text" ref="input1"/>{/* 官方不建议这么写 */}
+        <input type="text" ref={ input => this.input1 = input } />{/* 建议写法 */}
+        <button onClick={this.btnFn}>{this.state.btnText}</button>
+        <input onBlur={this.blurFn} type="text" ref={ input => this.input2 = input } />
+      </div>
     );
   }
 }
