f24fb75a5961963ba519a5d29d8f5af8a8f863fb
chen8000 <13126618000@163.com>
2018-07-18 22:21:18 +0800

    笔记

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -4,7 +4,4 @@ import ReactDOM from 'react-dom';
 // 自定义组建
 import App from './components/App';
 
-import registerServiceWorker from './registerServiceWorker';
-
 ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
