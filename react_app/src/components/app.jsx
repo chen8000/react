e6748b91a611d5a770f10a9f1dac979c76a6ad8c
chen8000 <13126618000@163.com>
2018-07-19 01:23:06 +0800

    完成

--- react_app/src/components/app.jsx
+++ react_app/src/components/app.jsx
@@ -6,17 +6,56 @@ import ContainerLeft from './containerLeft';
 import ContainerRight from './containerRight';
 
 export default class App extends Component {
+
+    constructor(props){
+        super(props)
+
+        this.state = {
+            list:[
+                {
+                    name:'哈哈哈',
+                    content:'react太难了'
+                }
+            ]
+        }
+
+        this.childUpdateFn = this.childUpdateFn.bind(this);
+        this.removeFn = this.removeFn.bind(this);
+    }
+
+    // 修改列表数据
+    childUpdateFn(val){
+
+        let list = this.state.list;
+        list.push(val);
+
+        // 修改数据
+        this.setState({ list });
+    }
+
+    // 删除数据
+    removeFn(index){
+        let { list } = this.state;
+        
+        delete list[index];
+
+        this.setState({ list });
+    }
+
+
     render(){
+        let list = this.state.list;
+
         return (
             <div>
                 <Header />
 
                 <div className="container">
                     <div className="containerLeft">
-                        <ContainerLeft />
+                        <ContainerLeft updateFn={this.childUpdateFn}/>
                     </div>
                     <div className="containerRight">
-                        <ContainerRight />
+                        <ContainerRight removeFn={this.removeFn} result={list}/>
                     </div>
                 </div>
 
--- react_app/src/components/containerRight.jsx
+++ react_app/src/components/containerRight.jsx
@@ -1,16 +1,33 @@
 import React, { Component } from 'react';
 
 export default class ContainerRight extends Component {
+
+    constructor(props){
+        super(props)
+
+        this.removeFn = this.removeFn.bind(this);
+    }
+
+    removeFn(event){
+        let index = event.target.parentNode.getAttribute('index');
+        
+        this.props.removeFn(index);
+    }
     
     render(){
         return(
             <div className="containerRightBox">
                 <h2>评论回复：</h2>
-                <div className="reply">
-                    <h3 className="title">xxx说：</h3>
-                    <p>xxxxxx</p>
-                    <button>删除</button>
-                </div>
+                
+                { this.props.result.map( (res, index) => { 
+                    return (
+                        <div key={index} index={index} className="reply">
+                            <h3 className="title">{res.name}：</h3>
+                            <p>{res.content}</p>
+                            <button onClick={this.removeFn}>删除</button>
+                        </div>
+                    )
+                 } ) }
             </div>
         )
     }
