7e42c9a53ce9a627725f1661b5b1256683f860d3
chen8000 <13126618000@163.com>
2018-07-20 23:19:47 +0800

    使用axios发送git请求

---
+++ axios/src/components/axios.jsx
@@ -0,0 +1,32 @@
+
+
+
+import React, { Component } from 'react';
+
+
+
+
+class Axios extends Component {
+
+    componentDidMount(){
+        
+    }
+
+    render(){
+        return (
+            <div>divvvv</div>
+        )
+    }
+}
+
+
+export default Axios;
+
+
+
+
+
+
+
+
+
---
+++ axios/src/index.js
@@ -0,0 +1,19 @@
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import Axios from './components/axios';
+
+
+ReactDOM.render(<Axios />, document.getElementById('root'));
+
+
+
+
+
+
+
+
+
+
+
