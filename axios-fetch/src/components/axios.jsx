ebdb8cf658f68d01518658ee792d5626623cc463
chen8000 <13126618000@163.com>
2018-07-21 00:15:20 +0800

    返回错误信息

--- axios-fetch/src/components/axios.jsx
+++ axios-fetch/src/components/axios.jsx
@@ -12,17 +12,29 @@ class Axios extends Component {
     componentDidMount(){
 
         // 使用axios发送get请求
-        axios.get('https://api.github.com/search/repositories?q=react&sort=starts').then( (res) => {
+        axios
+        .get('https://api.github.com/search/repositories?q=react&sort=starts')
+        .then( (res) => {
             console.log('axios');
             console.log(res);
-        });
+        })
+        // 返回错误信息
+        .catch( (err) => {
+            console.log(err)
+        })
+
 
         // 使用fetch发送异步请求
-        fetch('https://api.github.com/search/repositories?q=react&sort=starts').then( (res) => {
+        fetch('https://api.github.com/search/repositories?q=react&sort=starts')
+        .then( (res) => {
             console.log('fetch');
             return res.json()
-        }).then( (res) => {
-            console.log(res)
+        })
+        .then( (res) => {
+            console.log(res);
+        })
+        .catch( (err) => {
+            console.log(err);
         })
     }
 
