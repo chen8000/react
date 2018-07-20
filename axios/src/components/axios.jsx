af73698c7bf47ab0105d38a5eaf4c5288bd5c0d1
chen8000 <13126618000@163.com>
2018-07-21 00:07:09 +0800

    使用fetch发送get请求

--- axios/src/components/axios.jsx
+++ axios/src/components/axios.jsx
@@ -2,6 +2,7 @@
 
 
 import React, { Component } from 'react';
+import axios from 'axios';
 
 
 
@@ -9,7 +10,20 @@ import React, { Component } from 'react';
 class Axios extends Component {
 
     componentDidMount(){
-        
+
+        // 使用axios发送get请求
+        axios.get('https://api.github.com/search/repositories?q=react&sort=starts').then( (res) => {
+            console.log('axios');
+            console.log(res);
+        });
+
+        // 使用fetch发送异步请求
+        fetch('https://api.github.com/search/repositories?q=react&sort=starts').then( (res) => {
+            console.log('fetch');
+            return res.json()
+        }).then( (res) => {
+            console.log(res)
+        })
     }
 
     render(){
