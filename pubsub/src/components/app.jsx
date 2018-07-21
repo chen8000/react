86a5b973c89e5b8c7dd1e5e895d58ee163240de1
chen8000 <13126618000@163.com>
2018-07-21 15:09:00 +0800

    订阅发布

---
+++ pubsub/src/components/app.jsx
@@ -0,0 +1,18 @@
+
+
+import React, { Component } from 'react';
+
+class App extends Component {
+
+    render(){
+        return (
+            <div>div</div>
+        )
+    }
+}
+
+export default App;
+
+
+
+
