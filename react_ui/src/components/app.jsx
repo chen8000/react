d41b02dcaaca90c8a6e00fa9a2ebfe846252d0bd
chen8000 <13126618000@163.com>
2018-07-21 22:08:31 +0800

    配置antd按需打包

--- react_ui/src/components/app.jsx
+++ react_ui/src/components/app.jsx
@@ -1,12 +1,18 @@
 
 import React, {Component} from 'react'
+import { Button, Toast } from 'antd-mobile'
+import 'antd-mobile/dist/antd-mobile.css'
 
 class App extends Component {
+
+    handleClick = () => {
+        Toast.info('哈哈')
+    }
 
     render(){
         return (
             <div>
-                app
+                <Button type="primary" onClick={this.handleClick}>button</Button>
             </div>
         )
     }
